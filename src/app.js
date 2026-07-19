8a73e78b5b8e4aa98330811e76802112f89770d8
Magnitinka <magnitinka@yandex.ru>
2026-07-19 23:30:34 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,17 +3,20 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const calcRoutes = require('./routes/calc');
-const paymentRoutes = require('./routes/payment');
-const resultRoutes = require('./routes/result');
+// Подключаем только нужный роут result2
+const resultRoutes = require('./routes/result2');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/calc', calcRoutes);
-app.use('/api/payment', paymentRoutes);
+// Временно отключаем calc и payment, чтобы избежать ошибок
+// const calcRoutes = require('./routes/calc');
+// const paymentRoutes = require('./routes/payment');
+// app.use('/api/calc', calcRoutes);
+// app.use('/api/payment', paymentRoutes);
+
 app.use('/api/result', resultRoutes);
 
 app.get('/health', (req, res) => {
