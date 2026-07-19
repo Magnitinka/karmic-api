4e3addb50ace2bc46270c020ee865261b446686b
Magnitinka <magnitinka@yandex.ru>
2026-07-20 00:43:12 +0300

    Update app.js

--- src/services/app.js
+++ src/services/app.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 // Подключаем роут result2 (или result, если хочешь)
-const resultRoutes = require('../routes/result2');
+const resultRoutes = require('../routes/result3');
 app.use('/api/result', resultRoutes);
 
 app.get('/health', (req, res) => {
