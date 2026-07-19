687f4ca1a2e1a34bc2ff34492dd4807aef50b9f3
Magnitinka <magnitinka@yandex.ru>
2026-07-19 21:11:31 +0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const calcRoutes = require('./routes/calc');
+const paymentRoutes = require('./routes/payment');
+const resultRoutes = require('./routes/result');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/calc', calcRoutes);
+app.use('/api/payment', paymentRoutes);
+app.use('/api/result', resultRoutes);
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+module.exports = app;
